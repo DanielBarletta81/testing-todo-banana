163fa514e05afd924ae7f86cc36978c5f35de520
Daniel Barletta <101023275+DanielBarletta81@users.noreply.github.com>
2022-08-31 18:59:25 -0400

    Uploading initial files for Todo List app /test

---
+++ src/form.js
@@ -0,0 +1,24 @@
+// user input - includes validation
+import React, { useState } from "react";
+
+export function TodoForm({addTodo}){
+  const [value,setValue] = useState('');
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
---
+++ src/todo.js
@@ -0,0 +1,7 @@
+export function Todo({todo,index,remove}){
+  function handle(){
+    console.log('Ping:',index);
+    remove(index);
+  }
+  return <div className="todo" onClick={handle}>{todo.text} (-)</div>
+}
